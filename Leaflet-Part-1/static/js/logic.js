9c4223f05a7f7a66ec4014d7241a4b245a1696cd
vasquezr8 <vasquezr8@gmail.com>
2023-09-23 17:01:38 -0700

    Added circle markers

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,65 @@
+// Store our API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+
+// Perform a GET request to the query URL.
+d3.json(queryUrl).then(function (data) {
+
+  console.log(data.features);
+
+  // Using the features array sent back in the API data, create a GeoJSON layer, and add it to the map.
+
+  function useFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+
+  let earthquakes = L.geoJSON(data.features, {
+
+    onEachFeature: useFeature,
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng);
+    }
+  })
+
+  createMap(earthquakes);
+});
+
+
+function createMap(earthquakes) {
+  // Create the base layers.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  // Create a baseMaps object.
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
+
+  // Create an overlays object.
+  let overlayMaps = {
+    Earthquakes: earthquakes,
+  };
+  
+  // Create a new map.
+  // Edit the code to add the earthquake data to the layers.
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [street, earthquakes]
+  });
+
+  // Create a layer control that contains our baseMaps.
+  // Be sure to add an overlay Layer that contains the earthquake GeoJSON.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+}
